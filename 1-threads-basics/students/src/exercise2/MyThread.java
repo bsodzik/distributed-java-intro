dd098aa49d6c16ba170506a2171f4159c06ba4d1
heolin123 <heolin123@gmail.com>
2014-10-09 09:17:46 +0200

    Finished exercise1-4

--- 1-threads-basics/students/src/exercise2/MyThread.java
+++ 1-threads-basics/students/src/exercise2/MyThread.java
@@ -1,4 +1,23 @@
 package exercise2;
 
-public class MyThread {
+public class MyThread extends Thread {
+	public MyThread(String name) {
+		super(name);
+	}
+	
+	@Override
+	public void run()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.print(this.getName()+'\n');
+			try {
+				Thread.sleep(10);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}		
+		}
+		super.run();
+	}
 }
--- 1-threads-basics/students/src/exercise3/MyRunnable.java
+++ 1-threads-basics/students/src/exercise3/MyRunnable.java
@@ -1,4 +1,12 @@
 package exercise3;
 
-public class MyRunnable {
+public class MyRunnable implements Runnable {
+
+	public void run()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.print(Thread.currentThread().getName()+'\n');
+		}
+	}
 }
