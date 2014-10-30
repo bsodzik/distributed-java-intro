d0b1dbc71a1d61142fc9e068a87d111b7da8e8c0
mateusz.jancy <mateusz.jancy@gmail.com>
2014-10-30 09:15:50 +0100

    Initial content for lab 4. Solution for 1

--- 4-JMS-basics/SayHello/Say/src/main/java/pl/edu/amu/dji/jms/lab1/SayMain.java
+++ 4-JMS-basics/SayHello/Say/src/main/java/pl/edu/amu/dji/jms/lab1/SayMain.java
@@ -12,40 +12,23 @@ public class SayMain {
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        Connection connection = connectionFactory.createConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination queue = session.createQueue("SayHelloQueue");
+        MessageProducer producer = session.createProducer(queue);
 
-        /*
-        Create Connection instance from ConnectionFactory
-
-        Create Session instance from connection object.
-        - Session shouldn't by transactional and should by in auto acknowledge mode (Session.AUTO_ACKNOWLEDGE).
-
-        Create Destination queue from session (check Session class and createQueue method)
-        - queue name should be "SayHelloQueue"
-
-        Create MessageProducer from session for given queue/topic
-        - set producer delivery mode to non persistent (DeliveryMode.NON_PERSISTENT);
-         */
-
-        Connection connection = null;
-        Session session = null;
-        Destination queue = null;
-        MessageProducer producer = null;
-
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 
         connection.start();
-
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String text = "";
-        while (!text.equalsIgnoreCase(EXIT)) {
+        String in = "";
+        while (!in.equalsIgnoreCase(EXIT)) {
             System.out.print("Say hello to:");
-            text = bufferedReader.readLine();
-
-            //Create TextMessage from session with text variable
-            //Send this message to queue (use producer for that)
+            in = bufferedReader.readLine();
+            TextMessage message = session.createTextMessage(in);
+            producer.send(message);
         }
 
-        //Close stuff
         session.close();
         connection.close();
     }
